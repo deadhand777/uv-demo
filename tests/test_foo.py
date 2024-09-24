55bf077ce19b4d9408eb97ed68a3720dd5252cf1
deadhand777 <47255277+deadhand777@users.noreply.github.com>
2024-09-24 14:52:00 +0200

    fix tests

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,5 +1,5 @@
 from uv_demo.foo import foo
 
 
-def test_foo():
+def test_bar():
     assert foo("foo") == "foo"
--- uv_demo/foo.py
+++ uv_demo/foo.py
@@ -1,38 +1,18 @@
-import argparse
-from dataclasses import dataclass
-
 import awswrangler as wr
 import loguru
 
-print("argparse version: ", argparse.__version__)
-print(f"awswrangler version: {wr.__version__}")
-print(f"loguru version: {loguru.__version__}")
-
-parser = argparse.ArgumentParser()
-parser.add_argument("bar", type=str, default="foo")
-parser.parse_args()
+loguru.logger.info(wr.__version__)
 
 
-@dataclass()
-class Foo:
-    bar: str
+def foo(bar: str) -> str:
+    """Summary line.
 
-    def foo(bar: str) -> str:
-        """Summary line.
+    Extended description of function.
 
-        Extended description of function.
+    Args:
+        bar: Description of input argument.
 
-        Args:
-            bar: Description of input argument.
-
-        Returns:
-            Description of return value
-        """
-        loguru.logger.info(f"awswrangler version: {wr.__version__}")
-        loguru.logger.info("Hello World!")
-        loguru.logger.info(bar)
-        return bar
-
-
-if __name__ == "__main__":
-    Foo(bar=parser.parse_args().bar).foo()
+    Returns:
+        Description of return value
+    """
+    return bar
